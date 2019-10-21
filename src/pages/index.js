0f59dac5bfac6bb45ed900de846d20c700c32811
Clare <chblackburne@gmail.com>
2019-10-21 21:29:42 +0100

    basic index page for posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <ul>
+    {data.allStrapiPost.edges.map(post => (
+      <li key={post.node.id}>
+        <Link to={`/${post.node.id}`}>
+          <h3>{post.node.title}</h3>
+        </Link>
+        <p>{post.node.content}</p>
+        <Img fixed={post.node.banner.childImageSharp.fixed} />
+      </li>
+    ))}
+    </ul>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allStrapiPost {
+      edges {
+        node {
+          id
+          title
+          content
+          banner {
+            childImageSharp {
+              fixed(width: 200, height: 200){
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
